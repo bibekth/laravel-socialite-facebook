bb1e2b735c28f360f332315594aae3f941fc6737
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-02-28 19:10:32 +0545

    testing in live serve

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/facebook-login',[App\Http\Controllers\LoginController::class, 'login'])->name('facebook.login');
-Route::post('/facebook-login-redirect',[App\Http\Controllers\LoginController::class, 'redirect'])->name('facebook.redirect');
+Route::get('/facebook-login-redirect',[App\Http\Controllers\LoginController::class, 'redirect'])->name('facebook.redirect');
 Route::get('/privacy-policy',[App\Http\Controllers\LoginController::class, 'privacyPolicy'])->name('privacy.policy');
 Route::post('/facebook-delete-user',[App\Http\Controllers\LoginController::class, 'deleteFbUser'])->name('delete.fb.user');
 Route::get('/users', function(){
