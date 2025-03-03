bce2f8f0c00c2d2db18b6802a716ae2fd9eb583f
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-03-03 15:21:26 +0545

    changes

--- resources/views/showpost.blade.php
+++ resources/views/showpost.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ $post->title }}</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -16,7 +17,7 @@
             </div>
             @if ($post->banner_image)
             <div class="banner-section">
-                <img src="{{ asset('storage/'.$post->banner_image) }}" alt="">
+                <img src="{{ asset('storage/'.$post->banner_image) }}" alt="Banner">
             </div>
             @endif
             @if(!empty($post->description))
@@ -46,5 +47,9 @@
         </div>
     </div>
 </body>
-
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
 </html>
