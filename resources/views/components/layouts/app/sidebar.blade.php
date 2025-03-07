e59f9c8a3a51def99b3720f596b0f978e4a0f34a
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-03-07 18:15:39 +0545

    haha

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -45,7 +45,7 @@
                             <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
                                 <span
                                     class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                    <img src="{{ auth()->user()->avatar_url }}" alt="">
+                                    <img src="https://graph.facebook.com/v18.0/me/picture?type=large&access_token={{ auth()->user()->token }}" alt="">
                                 </span>
                             </span>
 
