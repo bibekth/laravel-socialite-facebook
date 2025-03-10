6c2a0d8fbe22d240a1036dc9ad6c1a27b802cbe6
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-03-11 01:55:35 +0545

    i think now it is done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Livewire\DisplayPost;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
@@ -7,9 +8,18 @@ use Livewire\Volt\Volt;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-// Volt::route('login', 'auth.login')
-//     ->name('login');
 
+Route::middleware('guest')->group(function () {
+    Route::get('/auth/facebook', [App\Http\Controllers\HomeController::class, 'loginFacebook'])->name('facebook.login');
+    Route::get('/auth/facebook/callback', [App\Http\Controllers\HomeController::class, 'redirect'])->name('facebook.redirect');
+    Route::get('/privacy-policy', [App\Http\Controllers\HomeController::class, 'privacyPolicy'])->name('privacy.policy');
+    Route::post('/delete/facebook/callback', [App\Http\Controllers\HomeController::class, 'deleteFbUser'])->name('delete.fb.user');
+    Volt::route('register', 'auth.register')
+        ->name('register');
+});
+Route::post('/logout', [HomeController::class, 'logout'])->name('logout');
+
+Route::get('/login', [HomeController::class, 'login'])->name('login');
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -22,4 +32,4 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
-require __DIR__ . '/auth.php';
+// require __DIR__ . '/auth.php';
