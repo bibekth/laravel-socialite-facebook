cf1cfb4bd2dedbc1858108293f9830611bb2aec6
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-04-03 12:03:08 +0545

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -165,7 +165,7 @@
                 <flux:heading size="xl">Your Posts</flux:heading>
                 <flux:subheading>List of the posts that you have created.</flux:subheading>
             </div>
-            @if(count(auth()->user()->posts) > 0)
+            {{-- @if(count(auth()->user()->posts) > 0) --}}
             <table>
                 <thead>
                     <tr>
@@ -190,11 +190,7 @@
 
                 </tbody>
             </table>
-            @endif
-            {{-- @foreach(auth()->user()->posts as $post)
-            <div class=""><a href="{{ asset($post->slug) }}" target="_blank" rel="noopener noreferrer">{{
-                    $loop->iteration }} . {{ $post->title }}</a></div>
-            @endforeach --}}
+            {{-- @endif --}}
         </div>
     </div>
     @endvolt
